ae0550c597109304c128a7ba42993b790c943889
Md Shahed Talukder <35971677+shahed-swe@users.noreply.github.com>
2021-02-16 19:05:20 +0600

    new commit

--- object_in_js.js
+++ object_in_js.js
@@ -39,3 +39,21 @@ function swap(x) {
 }
 swap(obj)
 console.log("After Swap A: " + obj.a + " B: " + obj.b);
+// as we can see in objects the values of that objects changed permanently because we have use the 
+// reference of that object and not using the value direct.
+// now we will practise some code on our console
+// we will just write them here for some practise
+var arr = [0, 1, 5, 7, 3, 4]
+console.log(arr)
+console.log(arr.length)
+    // for being a proto object we can access Array properties by it. also we can use __proto__ methods like that
+arr.push(100, 120, 125)
+console.log(arr)
+    // we can see the index numbers by doing this on an array or an object
+    // but this will work only on our browser cause in our node we will only see those elements only
+console.dir(arr)
+arr.pop()
+console.log(arr)
+    // now we will see the __proto__ of an array and the Array.protype is equal or not
+console.log(arr.__proto__ == Array.prototype)
+    // we can see both are same
